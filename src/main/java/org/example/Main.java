1e959c25ae32fda1ce13e3af74123e18d3ecb598
본ìBeconfident01 <hom0726@naver.com>
2023-03-02 09:23:45 +0900

    1단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("==명언 앱==");
+        Scanner sc =new Scanner(System.in);
+        while(true) {
+            System.out.print("명령) ");
+            String answer =sc.nextLine();
+            if(answer.equals("종료")){
+                break;
+
+            }
+        }
+        sc.close();
     }
+
 }
