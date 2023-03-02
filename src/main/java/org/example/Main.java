7ec9bd7e6f46cdc63a18333632234945f3412177
본ìBeconfident01 <hom0726@naver.com>
2023-03-02 09:46:07 +0900

    4단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("==명언 앱==");
         Scanner sc =new Scanner(System.in);
+        int i=1;
         while(true) {
             System.out.print("명령) ");
-            String answer =sc.nextLine();
 
-            if(answer.equals("등록")){
-                System.out.println("명언 : 현재를 사랑하라.");
-                System.out.println("작가 : 작자미상");
-                System.out.println("1번 명언이 등록되었습니다.");
+            String answer =sc.nextLine();
+            if(answer.equals("등록")) {
+                    System.out.println("명언 : 현재를 사랑하라.");
+                    System.out.println("작가 : 작자미상");
+                    System.out.println(i + "번 명언이 등록되었습니다.");
+                i++;
             }
+
+
+
 
             if(answer.equals("종료")){
                 break;
-
             }
+
         }
         sc.close();
     }
